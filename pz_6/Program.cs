1cbeabf81c6a579a227d314293765b42802843f5
Коленов Антон <kolenov00@internet.ru>
2022-09-27 22:44:11 +0500

    pz_6

---
+++ pz_6/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace pz_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите целое число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int k = 0;
+            while (n > 1)
+            {
+                n /= 2;
+                k++;
+            }
+            Console.WriteLine($"Показатель степени: {k}");
+            Console.ReadKey();
+        }
+    }
+}
