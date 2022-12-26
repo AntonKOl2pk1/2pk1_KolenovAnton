4dcca1bd9befa310ff5aa7ed9a18a8431456cf3e
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2022-12-26 13:01:45 +0500

    13 практика

---
+++ pz13/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace pz13
+{
+    internal class Program
+    {
+        /*Реализуйте метод, принимающий в качестве параметра строку, содержащую некоторую
+формулу. Метод возвращает true либо false в зависимости от того, является ли строка
+корректной формулой (будем считать ее корректной, если количество открывающих и
+закрывающих скобок одинаково)
+*/
+
+        static void Math(string stroka)
+        {
+            char[] chars = stroka.ToCharArray();
+            
+            int count1 = 0;
+            int count2 = 0;
+
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == '(')
+                {
+                    count1++;
+                }
+                if (chars[i] == ')')
+                {
+                    count2++;
+                }
+            }
+
+            if (count1 == count2)
+            {
+                Console.WriteLine("Строка введена верно");
+            }
+            else
+            {
+                Console.WriteLine("Cтрока введена неверно");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите математическое выражение: ");
+            string stroka = Console.ReadLine();
+            Math(stroka);
+
+        }
+    }
+}
