3a9f486e6564e88c23741460a1f3f5c661d0a2fc
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2022-12-26 12:58:22 +0500

    8 практика

---
+++ pz8/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace pz8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Вычислить сумму элементов каждого столбца матрицы (двумерного массива).
+
+            Console.Write("Введите число: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[,] arr = new int[n, n];
+            Random rnd = new Random();
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    arr[i, j] = rnd.Next(100);
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                    Console.Write(arr[i, j] + " ");
+                Console.WriteLine();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                int sum = 0;
+                for (int j = 0; j < n; j++)
+                {
+                    sum += arr[j, i];
+                }
+                Console.WriteLine($"Сумма в столбце {i} равна {sum}");
+            }
+
+        }
+    }
+}
