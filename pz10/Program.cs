eafd3961bc2b421c55a10e1af5d811bec10e61b7
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2022-12-26 12:59:37 +0500

    10 практика

---
+++ pz10/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace pz10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Дан символ C и строка S. Удвоить каждое вхождение символа C в строке S
+
+            Console.Write("Введите символ: ");
+            char symbol = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Введите строку: ");
+            string stroka = Console.ReadLine();
+
+            char[] array = stroka.ToCharArray();
+            for(int i = 0; i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == symbol)
+                {
+                    stroka = stroka.Insert(i, symbol.ToString());
+                }
+            }
+            Console.WriteLine(stroka);
+
+        }
+    }
+}
