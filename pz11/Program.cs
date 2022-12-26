9050a591229a2d5f84294b61be0d028c3a7990ec
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2022-12-26 13:00:16 +0500

    11 практика

---
+++ pz11/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace pz11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*В произвольном тексте осуществить сортировку предложений по количеству
+            символов в порядке возрастания*/
+
+            string text = 
+                "\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." +
+                "\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur." +
+                "\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+            string[] array = text.Split('\n');
+            int maxlen = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                Array.Sort(array);
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}
