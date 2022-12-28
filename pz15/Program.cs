8cc74d714af796c987b9c8ace8d5a32e2318d417
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2022-12-28 12:35:33 +0500

    15 практика

---
+++ pz15/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace pz15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string emptyString = "";
+            
+            StreamReader NewFile = new StreamReader("test.txt");
+            while (NewFile.EndOfStream != true)
+            {
+                emptyString += NewFile.ReadToEnd(); //присваиваем весь текст из файла
+            }
+            string[] array = emptyString.Split('\n');
+
+            for (int i = 0; i < array.Length; i++)
+            { 
+
+               //int index = array.IndexOf(i);
+                if (i % 2 == 0 )
+                {
+                    array[i] = "";
+                    
+                }
+                Console.WriteLine(array[i]);
+
+            }
+            
+            NewFile.Close();
+
+        }
+    }
+}
