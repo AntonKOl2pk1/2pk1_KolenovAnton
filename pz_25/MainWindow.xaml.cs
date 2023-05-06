9ec85d54dd8ba099c27fe11299886c2dc68a9bc3
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2023-05-06 18:46:01 +0500

    25 практика

---
+++ pz_25/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace pz_25
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            foreach (UIElement element in Calculator.Children)
+            {
+                if (element is Button)
+                {
+                    ((Button)element).Click += Button_Click;
+                }
+            }
+
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string str = (string)((Button)e.OriginalSource).Content;
+
+            if (str == "AC")
+            {
+                Result.Text = "";
+            }
+            else if (str == "=")
+            {
+                string value = new DataTable().Compute(Result.Text, null).ToString();//
+
+                Result.Text = value;
+            }
+            else
+            {
+                Result.Text += str;
+            }
+        }
+    }
+}
