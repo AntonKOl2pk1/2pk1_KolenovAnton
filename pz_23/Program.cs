8e36c935ea7bf34ebeb634f8cf3391c81adc47c5
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2023-05-06 18:44:31 +0500

    23 практика

---
+++ pz_23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCard book1 = new BookCard(1, "Агата Кристи", "Убийство в «Восточном экспрессе»", 1934, BookCard.BookCondition.Утилизация);
+            BookCard book2 = new BookCard(2, "Оруэлл Джордж", "1984", 1949, BookCard.BookCondition.Отличное);
+            BookCard book3 = new BookCard(3, "Харпер Ли", "Убить пересмешника", 1960, BookCard.BookCondition.Новая);
+            book1.GetBookInfo();
+            book2.GetBookInfo();
+            book3.GetBookInfo();
+            ScientificBookCard newbook1 = new ScientificBookCard(1, "Linus Pauling", "General Chemistry", 1988, BookCard.BookCondition.Отличное, ScientificBookCard.BookDirection.Химия, new DateTime(2019, 1, 1));
+            ScientificBookCard newbook2 = new ScientificBookCard(2, "Stephen Hawking", "A Brief History of Time", 1988, BookCard.BookCondition.Новая, ScientificBookCard.BookDirection.Физика, new DateTime(2021, 5, 1));
+
+            newbook1.GetBookInfo();
+            newbook2.GetBookInfo();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ pz_23/ScientificBookCard.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_23
+{
+    public class ScientificBookCard : BookCard
+    {
+        public enum BookDirection
+        {
+            Химия,
+            Медицина,
+            Физика,
+            Математика,
+            Биология
+        }
+        public BookDirection Direction;
+        public DateTime ReceiptDate
+        {
+            get { return receiptDate; }
+            set
+            {
+                if (DateTime.Now.Year - value.Year <= 5)
+                {
+                    receiptDate = value;
+                }
+                else
+                {
+                    Console.WriteLine("Дата получения не может быть более 5 лет назад.");
+                }
+            }
+        }
+
+        private DateTime receiptDate;
+
+        public ScientificBookCard(int id, string author, string title, int year, BookCondition condition, BookDirection direction, DateTime receiptDate) : base(id, author, title, year, condition)
+        {
+            Direction = direction;
+            ReceiptDate = receiptDate;
+        }
+
+        public new void GetBookInfo()
+        {
+            base.GetBookInfo();
+            Console.WriteLine($"Направление: {Direction}");
+            Console.WriteLine($"Дата получения: {ReceiptDate.ToShortDateString()}");
+        }
+    }
+
+}
