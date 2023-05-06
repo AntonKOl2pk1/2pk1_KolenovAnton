1ba1b7e1a3d1c6e8119cd43a3f9ff3a5dd97ffdf
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2023-05-06 18:43:22 +0500

    22 практика

---
+++ pz_22/BookCard.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    public class BookCard
+    {
+        private int ID;
+        private string author;
+        private string title;
+        private int year;
+        private string condition;
+
+        public enum BookCondition
+        {
+            Утилизация,
+            Отличное,
+            Новая
+        }
+
+        public static int ToBeDisposedCount { get; private set; }
+        public static int GoodCount { get; private set; }
+        public static int NewCount { get; private set; }
+
+        public BookCard(int id, string author, string title, int year, BookCondition condition)
+        {
+            ID = id;
+            Author = author;
+            Title = title;
+            Year = year;
+            Condition = condition;
+            UpdateCounters();
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Автор не может быть пустым");
+                author = value;
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Заголовок не может быть пустым");
+                title = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1990)
+                    Console.WriteLine("Год не может быть ранее 1990-го");
+                year = value;
+            }
+        }
+
+        public BookCondition Condition
+        {
+            get { return (BookCondition)Enum.Parse(typeof(BookCondition), condition); }
+            set
+            {
+                condition = value.ToString();
+                UpdateCounters();
+            }
+        }
+
+        private void UpdateCounters()
+        {
+            switch (Condition)
+            {
+                case BookCondition.Утилизация:
+                    ToBeDisposedCount++;
+                    break;
+                case BookCondition.Отличное:
+                    GoodCount++;
+                    break;
+                case BookCondition.Новая:
+                    NewCount++;
+                    break;
+            }
+        }
+
+        public void GetBookInfo()
+        {
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Author: {0}", Author);
+            Console.WriteLine("Title: {0}", Title);
+            Console.WriteLine("Year: {0}", Year);
+            Console.WriteLine("Condition: {0}", Condition);
+        }
+    }
+
+}
---
+++ pz_22/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCard book1 = new BookCard(1, "Агата Кристи", "Убийство в «Восточном экспрессе»", 1934, BookCard.BookCondition.Утилизация);
+            BookCard book2 = new BookCard(2, "Оруэлл Джордж", "1984", 1949, BookCard.BookCondition.Отличное);
+            BookCard book3 = new BookCard(3, "Харпер Ли", "Убить пересмешника", 1960, BookCard.BookCondition.Новая);
+            book1.GetBookInfo();
+            book2.GetBookInfo();
+            book3.GetBookInfo();
+            Console.ReadKey();
+        }
+    }
+}
