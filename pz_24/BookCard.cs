54dacf77e1f35fbcf3ee579493437eb66d684a1d
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2023-05-06 18:45:25 +0500

    24 практика

---
+++ pz_24/BookCard.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_24
+{
+    internal class BookCard: IClonable
+    {
+        public int ID;
+        public string author;
+        public string title;
+        public int year;
+        public string condition;
+
+        public enum BookCondition
+        {
+            Утилизация,
+            Отличное,
+            Новая
+        }
+
+        public static int ToBeDisposedCount { get; private set; }
+        public static int GoodCount { get; private set; }
+        public static int NewCount { get; private set; }
+
+        public BookCard(int id, string author, string title, int year, BookCondition condition)
+        {
+            ID = id;
+            Author = author;
+            Title = title;
+            Year = year;
+            Condition = condition;
+            UpdateCounters();
+        }
+
+        public string Author
+        {
+            get { return author; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Автор не может быть пустым");
+                author = value;
+            }
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    Console.WriteLine("Заголовок не может быть пустым");
+                title = value;
+            }
+        }
+
+        public int Year
+        {
+            get { return year; }
+            set
+            {
+                if (value < 1990)
+                    Console.WriteLine("Год не может быть ранее 1990-го");
+                year = value;
+            }
+        }
+
+        public BookCondition Condition
+        {
+            get { return (BookCondition)Enum.Parse(typeof(BookCondition), condition); }
+            set
+            {
+                condition = value.ToString();
+                UpdateCounters();
+            }
+        }
+
+        private void UpdateCounters()
+        {
+            switch (Condition)
+            {
+                case BookCondition.Утилизация:
+                    ToBeDisposedCount++;
+                    break;
+                case BookCondition.Отличное:
+                    GoodCount++;
+                    break;
+                case BookCondition.Новая:
+                    NewCount++;
+                    break;
+            }
+        }
+
+        public void GetBookInfo()
+        {
+            Console.WriteLine("ID: {0}", ID);
+            Console.WriteLine("Author: {0}", Author);
+            Console.WriteLine("Title: {0}", Title);
+            Console.WriteLine("Year: {0}", Year);
+            Console.WriteLine("Condition: {0}", Condition);
+        }
+
+        public new object Clone()
+        {
+            return this.MemberwiseClone();
+        }
+    }
+}
---
+++ pz_24/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BookCard book1 = new BookCard(1, "Агата Кристи", "Убийство в «Восточном экспрессе»", 1934, BookCard.BookCondition.Утилизация);
+            BookCard book2 = new BookCard(2, "Оруэлл Джордж", "1984", 1949, BookCard.BookCondition.Отличное);
+            BookCard book3 = new BookCard(3, "Харпер Ли", "Убить пересмешника", 1960, BookCard.BookCondition.Новая);
+            book1.GetBookInfo();
+            book2.GetBookInfo();
+            book3.GetBookInfo();
+            Console.WriteLine();
+            Console.WriteLine();
+            ScientificBookCard newbook1 = new ScientificBookCard(1, "Linus Pauling", "General Chemistry", 1988, BookCard.BookCondition.Отличное, ScientificBookCard.BookDirection.Химия, new DateTime(2019, 1, 1));
+            ScientificBookCard newbook2 = new ScientificBookCard(2, "Stephen Hawking", "A Brief History of Time", 1988, BookCard.BookCondition.Новая, ScientificBookCard.BookDirection.Физика, new DateTime(2021, 5, 1));
+            newbook1.GetBookInfo();
+            newbook2.GetBookInfo();
+
+            book1.ID = 3;
+            book2.ID = 4;
+            book2.ID = 5;
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine($"Клонированные объекты с изменениями \nID книги 1: {book1.ID} \nID книги 2: {book2.ID} \nID книги 3: {book3.ID} ");
+            Console.ReadKey();
+        }
+    }
+}
