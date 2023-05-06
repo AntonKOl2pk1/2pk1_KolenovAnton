235d164c73855073d1b1d1b6fb213478e2f25f5b
AntonKOl2pk1 <114181514+AntonKOl2pk1@users.noreply.github.com>
2023-05-06 18:42:48 +0500

    21 практика

---
+++ pz_21/BookCard.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pz_21
+{
+    internal class BookCard
+    {
+        public int ID { get; set; } //Уникальный номер
+        public string Author { get; set; } //автор
+        public string Title { get; set; } //название
+        public int Year { get; set; } // Год издения экземпляра
+        public string Condition { get; set; } //Состояние книги
+
+        public void GetBookInfo()
+        {
+            Console.WriteLine($"ID: {ID}");
+            Console.WriteLine($"Author: {Author}");
+            Console.WriteLine($"Title: {Title}");
+            Console.WriteLine($"Year: {Year}");
+            Console.WriteLine($"Condition: {Condition}");
+        }
+
+    }
+}
